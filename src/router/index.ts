1dd3b771d4d000f5b736fe81b5bec76a65142fb2
MarkCherepovskyi <mark.cherepovskyi@gmail.com>
2024-03-23 00:20:23 +0200

    init template

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import {
+  createRouter,
+  createWebHistory,
+  RouteRecordRaw,
+  useRoute,
+  useRouter,
+} from 'vue-router'
+
+import { ROUTE_NAMES } from '@/enums'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/:catchAll(.*)',
+    redirect: { name: ROUTE_NAMES.home },
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  scrollBehavior: () => ({ top: 0, left: 0 }),
+})
+
+export { router, useRouter, useRoute }
